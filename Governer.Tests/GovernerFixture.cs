842b8bc3ead25329f8a7d79ff839e52aa7ee3936
Nikhil Prasad <nikhil.prasad@gmail.com>
2015-03-19 01:39:26 +0530

    Added governer implementation.

---
+++ Governer.Tests/GovernerFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Governer.Tests
+{
+	[TestFixture]
+	public class GovernerFixture
+	{
+		[Test]
+		public void GovernerTest ()
+		{
+			var governer = new Governer ("tenant-x-multi-avail", 25, new TimeSpan (0, 5, 0));
+			Assert.AreEqual (true, governer.IsAllowed ());
+		}
+
+		[Test]
+		public void ExceedingRateShouldNotBeAllowedTest ()
+		{
+			var governer = new Governer ("tenant-x-multi-avail", 0, new TimeSpan (0, 5, 0));
+			Assert.AreEqual (false, governer.IsAllowed ());
+		}
+	}
+}
+
---
+++ Governer/Governer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Governer
+{
+	public class Governer
+	{
+		public Governer (string name, int maxRatePerSecond, TimeSpan windowSize)
+		{
+			var windowSizeInSeconds = Convert.ToInt32(windowSize.TotalSeconds);
+			_gauge = new Gauge (name, windowSizeInSeconds);
+			_maxCount = windowSizeInSeconds * maxRatePerSecond;
+		}
+
+		private readonly Gauge _gauge;
+		private readonly long _maxCount = 0;
+		
+		public bool IsAllowed ()
+		{
+			var count = _gauge.Increment ();
+			return count <= _maxCount;
+		}
+	}
+}
+
