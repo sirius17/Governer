38087cbb79d2fefa63e7088839dafc2a268eb2d6
Nikhil Prasad <nikhil.prasad@gmail.com>
2015-03-19 22:44:46 +0530

    Added implementation for NtpTimeServer.

---
+++ Governer.Tests/NtpTimeServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Governer.Tests
+{
+	[TestFixture]
+	public class NtpTimeServerFixture
+	{
+		[Test]
+		public void GetNetworkTimeTest ()
+		{
+			var server = new NtpTimeServer ("pool.ntp.org");
+			var serverNow = server.GetCurrentUtcTime ();
+			var localNow = DateTime.UtcNow;
+			var difference = Convert.ToInt32(( serverNow - localNow ).Duration().TotalSeconds );
+			Console.WriteLine ("Server time: {0} and local time: {1}", serverNow, localNow);
+			Assert.IsTrue (difference < 30);
+		}
+	}
+}
+
